4b3864f517e9f6849c33a72636fd3fab9369afdb
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-10-04 14:17:18 -0700

    Skeleton code for view.

--- todo.rb
+++ todo.rb
@@ -1,3 +1,35 @@
 require_relative 'config/application'
 
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+def display_menu
+  puts
+  puts "*" * 100
+  puts "Usage:"
+  puts "ruby todo.rb list \t\t\t\t # List all tasks"
+  puts "ruby todo.rb add TASK \t\t\t\t # Add task to do e.g. ruby todo.rb Buy groceries"
+  puts "ruby todo.rb delete NUM_OF_TASK_TO_DELETE \t # Delete a task e.g. ruby todo.rb delete 1"
+  puts "ruby todo.rb delete NUM_OF_TASK_TO_DELETE \t # Complete a task e.g. ruby todo.rb complete 1"
+  puts
+end
+
+if ARGV.any?
+
+  case ARGV[0]
+    when "list"
+      puts "list"
+    when "add"
+      puts "add"
+    when "delete"
+      puts "delete"
+    when "complete"
+      puts "complete"
+    else
+      puts "invalid command"
+      display_menu
+  end
+
+else
+  display_menu
+end
+
+
+
