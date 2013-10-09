57a83fbc1d6ceedcc7c5d1bfc920259bfdba1d7e
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-10-09 14:09:00 -0700

    Dynamically load any controllers and views added.

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require 'pathname'
 require 'sqlite3'
 require 'active_record'
 require 'logger'
-require_relative '../app/controllers/tasks_controller'
 
 
 APP_ROOT = Pathname.new(File.expand_path(File.join(File.dirname(__FILE__), '..')))
@@ -23,6 +22,16 @@ end
 Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
   filename = File.basename(model_file).gsub('.rb', '')
   autoload ActiveSupport::Inflector.camelize(filename), model_file
+end
+
+Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each do |controller_file|
+  filename = File.basename(controller_file).gsub('.rb', '')
+  autoload ActiveSupport::Inflector.camelize(filename), controller_file
+end
+
+Dir[APP_ROOT.join('app', 'views', '*.rb')].each do |view_file|
+  filename = File.basename(view_file).gsub('.rb', '')
+  autoload ActiveSupport::Inflector.camelize(filename), view_file
 end
 
 ActiveRecord::Base.establish_connection :adapter  => 'sqlite3',
